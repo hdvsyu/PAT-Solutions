90f374decb244d5983ac253bbb4b1e7fa4570b4a
JoyHwong <hzy961165750@qq.com>
2016-02-22 16:18:06 +0800

    Basic Level 1033

--- Basic Level/basicLevel1033/Main.java
+++ Basic Level/basicLevel1033/Main.java
@@ -1,6 +1,6 @@
 package basicLevel1033;
 
-//have a test point failure.
+// 测试点4错误
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -13,33 +13,37 @@ public class Main {
 		String badKeyInput = in.nextLine();
 		String testKey = in.nextLine();
 		in.close();
-		Set<Character> badKeySet = new HashSet<>();
+		
+		// 判断上档键是否在坏掉的键盘里面
+		boolean noShiftKey = true;
+		Set<Character> set = new HashSet<>(); // 将坏掉的键放入散列表里面，方便查找
 		for (int i = 0; i < badKeyInput.length(); i++) {
-			badKeySet.add(badKeyInput.charAt(i));
+			char c = badKeyInput.charAt(i);
+			if (c =='+') {
+				noShiftKey = false;
+			}
+			set.add(c);
+			set.add(Character.toLowerCase(c));
 		}
 		
-		boolean isCanUpper = !badKeySet.contains('+');
-		
-		Set<Character> outputSet = new HashSet<>();
-		for (int i = 0; i < testKey.length(); i++) {
-			char c = testKey.charAt(i);
-			
-			if (isCanUpper) {
-				if (!badKeySet.contains(c)) {
+		if (noShiftKey) {
+			// 上档键不在坏掉的键里面，可以输出大写的
+			for (int i = 0; i < testKey.length(); i++) {
+				char c = testKey.charAt(i);
+				if (!set.contains(c)) {
 					System.out.print(c);
-					outputSet.add(c);
 				}
-			} else {
-				if (!Character.isUpperCase(c) && !badKeySet.contains(Character.toUpperCase(c))) {
+			}
+		} else {
+			// 上档键在坏掉的键，如果是大写的，则不输出
+			for (int i = 0; i < testKey.length(); i++) {
+				char c = testKey.charAt(i);
+				if (!Character.isUpperCase(c) && !set.contains(c)) {
 					System.out.print(c);
-					outputSet.add(c);
-				}
+				} 
 			}
 		}
 		
-		if (outputSet.size() == 0) {
-			System.out.println();
-		}
 	}
 
 }
