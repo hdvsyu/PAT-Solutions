52123cbac4db13f688975134271e0dce8207ac7c
JoyHwong <hzy961165750@qq.com>
2016-02-23 18:02:15 +0800

    Basic Level 1033

--- Basic Level/basicLevel1033/Main.java
+++ Basic Level/basicLevel1033/Main.java
@@ -13,19 +13,19 @@ public class Main {
 		String badKeyInput = in.nextLine();
 		String testKey = in.nextLine();
 		in.close();
-		
+
 		// 判断上档键是否在坏掉的键盘里面
 		boolean noShiftKey = true;
 		Set<Character> set = new HashSet<>(); // 将坏掉的键放入散列表里面，方便查找
 		for (int i = 0; i < badKeyInput.length(); i++) {
 			char c = badKeyInput.charAt(i);
-			if (c =='+') {
+			if (c == '+') {
 				noShiftKey = false;
 			}
 			set.add(c);
 			set.add(Character.toLowerCase(c));
 		}
-		
+
 		if (noShiftKey) {
 			// 上档键不在坏掉的键里面，可以输出大写的
 			for (int i = 0; i < testKey.length(); i++) {
@@ -38,12 +38,12 @@ public class Main {
 			// 上档键在坏掉的键，如果是大写的，则不输出
 			for (int i = 0; i < testKey.length(); i++) {
 				char c = testKey.charAt(i);
-				if (!Character.isUpperCase(c) && !set.contains(c)) {
-					System.out.print(c);
-				} 
+				if (!Character.isUpperCase(c)) {
+					if (!set.contains(c)) {
+						System.out.print(c);
+					}
+				}
 			}
 		}
-		
 	}
-
 }
