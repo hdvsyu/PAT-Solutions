de3aeca1710f0d6f32ea8f0bac84e90e6864c462
JoyHwong <hzy961165750@qq.com>
2016-07-30 13:11:38 +0800

    PAT A 1100

--- Advanced Level/advancedLevel1100/Main.java
+++ Advanced Level/advancedLevel1100/Main.java
@@ -9,39 +9,25 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-
-		// low digit.
 		final String[] lowDigit = { "tret", "jan", "feb", "mar", "apr", "may", "jun", "jly", "aug", "sep", "oct", "nov",
 				"dec" };
-
-		// high digit.
 		final String[] highDigit = { "", "tam", "hel", "maa", "huh", "tou", "kes", "hei", "elo", "syy", "lok", "mer",
 				"jou" };
-
-		// string to number
-		Map<String, Integer> map = new HashMap<>();
+		Map<String, Integer> map = new HashMap<String, Integer>();
 		for (int i = 0; i < lowDigit.length; i++) {
 			map.put(lowDigit[i], i);
-		}
-
-		for (int i = 0; i < highDigit.length; i++) {
 			map.put(highDigit[i], 13 + i);
 		}
-
 		int n = in.nextInt();
-
 		for (int i = 0; i < n; i++) {
 			int numInteger = 0;
 			String numString = null;
-
 			try {
 				numInteger = in.nextInt();
 			} catch (InputMismatchException inputMismatchException) {
 				numString = in.nextLine();
 			} finally {
-
 				if (numString == null) {
-					// the input is numbers.
 					if (numInteger < 13) {
 						System.out.println(lowDigit[numInteger]);
 					} else {
@@ -52,18 +38,10 @@ public class Main {
 						}
 					}
 				} else {
-					// the input is characters.
-					String[] tempString = numString.split(" "); // split the
-																// input
-
-					// if the length of tempString is 2, it must combine be high
-					// and low
+					String[] tempString = numString.split(" ");
 					if (tempString.length == 2) {
 						System.out.println((map.get(tempString[0]) - 13) * 13 + map.get(tempString[1]));
 					} else {
-						// maybe high or low
-
-						// map.get(Object) >= 13 is high
 						if (map.get(tempString[0]) >= 13) {
 							System.out.println((map.get(tempString[0]) - 13) * 13);
 						} else {
