100c6c0c57723697d4cbb3a4716c64e837174f5a
JoyHwong <hzy961165750@qq.com>
2016-07-31 17:08:26 +0800

    PAT A 1029

--- Advanced Level/advancedLevel1029/Main.java
+++ Advanced Level/advancedLevel1029/Main.java
@@ -1,4 +1,4 @@
-package advancedLevelpat1029;
+package advancedLevel1029;
 
 import java.util.Scanner;
 import java.util.Set;
@@ -7,9 +7,7 @@ import java.util.TreeSet;
 public class Main {
 	public static void main(String[] args) {
 		Set<Double> set = new TreeSet<>();
-		
 		Scanner in = new Scanner(System.in);
-		
 		int n1 = in.nextInt();
 		for (int i = 0; i < n1; i++) {
 			set.add(in.nextDouble());
@@ -19,14 +17,13 @@ public class Main {
 		for (int i = 0; i < n2; i++) {
 			set.add(in.nextDouble());
 		}
-		
 		in.close();
 		
-		int median = (n1 + n2 - 1) / 2;
+		int mid = (n1 + n2 - 1) / 2;
 		
 		int i = 0;
 		for (Double a : set) {
-			if (i == median) {
+			if (i == mid) {
 				System.out.printf("%.0f", a);
 				break;
 			}
