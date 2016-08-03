bf89bff258691f1721e6917c284e0aab9f9e13a0
JoyHwong <hzy961165750@qq.com>
2016-08-03 15:57:12 +0800

    PAT A 1007

--- Advanced Level/advancedLevel1007/Main.java
+++ Advanced Level/advancedLevel1007/Main.java
@@ -32,7 +32,7 @@ public class Main {
 
 	public static boolean havePositive(int[] nums) {
 		for (int i = 0; i < nums.length; i++) {
-			if (nums[i] > 0) {
+			if (nums[i] >= 0) {
 				return true;
 			}
 		}
