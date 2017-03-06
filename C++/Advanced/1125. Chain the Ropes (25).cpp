729d0fbe730e55eb025f5e08ac01de7c1788c812
JoyHwong <hzy961165750@qq.com>
2017-03-06 08:45:40 +0800

    PAT 1125

---
+++ C++/Advanced/1125. Chain the Ropes (25).cpp
@@ -0,0 +1,27 @@
+#include <queue>
+#include <cstdio>
+#include <functional>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n = 0;
+    scanf("%d", &n);
+    priority_queue<int, vector<int>, greater<int> > q;
+    for (int i = 0; i < n; i++) {
+        int temp = 0;
+        scanf("%d", &temp);
+        q.push(temp);
+    }
+    
+    while (q.size() > 1) {
+        int a = q.top();
+        q.pop();
+        int b = q.top();
+        q.pop();
+        
+        q.push((a + b) / 2);
+    }
+    printf("%d", q.top());
+}
