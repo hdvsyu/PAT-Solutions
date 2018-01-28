66e783a26723493ff5763e93c6b7da65d52aae5b
xiaorong <xiaorong61@gmail.com>
2018-01-28 12:46:23 +0800

    大写转换应放到和已经保存的坏键（bad 字符数组/字符串）对比时，而不是开始：
    如果一开始就先转换成大写，对于这种情况：
    aaaaA
    a
    会找不出坏键。

--- Java/Basic/basicLevel1029/Main.java
+++ Java/Basic/basicLevel1029/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String should = in.nextLine().toUpperCase();
-        String reality = in.nextLine().toUpperCase();
+        String should = in.nextLine();
+        String reality = in.nextLine();
         in.close();
-        String result = "";
+        String bad = "";
         for (int i = 0; i < should.length(); i++)
-            if (reality.indexOf(should.charAt(i)) == -1 && result.indexOf(should.charAt(i)) == -1)
-                result += should.charAt(i);
-        System.out.println(result);
+            if (reality.indexOf(should.charAt(i)) == -1 && bad.indexOf(Character.toUpperCase(should.charAt(i))) == -1)
+                bad += Character.toUpperCase(should.charAt(i));
+        System.out.println(bad);
     }
 }
