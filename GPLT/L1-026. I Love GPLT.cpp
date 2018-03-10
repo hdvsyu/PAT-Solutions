8efb5459b3c469cde39c3541ce01a68a277b1e14
hdvsyu <hdvsyu@foxmail.com>
2018-03-10 21:25:22 +0800

    feat: L1-026. I Love GPLT

---
+++ GPLT/L1-026. I Love GPLT.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+int main() {
+    printf("I\n");
+    printf(" \n");
+    printf("L\n");
+    printf("o\n");
+    printf("v\n");
+    printf("e\n");
+    printf(" \n");
+    printf("G\n");
+    printf("P\n");
+    printf("L\n");
+    printf("T\n");
+    return 0;
+}
