496d58a183e13ad2eb7d7827d9947a1a14de3eab
hdvsyu <hdvsyu@foxmail.com>
2018-04-02 08:45:49 +0800

    refactor: reduce code line

--- C++/Advanced/1009. Product of Polynomials (25).cpp
+++ C++/Advanced/1009. Product of Polynomials (25).cpp
@@ -1,46 +1,33 @@
-#include <cstring>
 #include <cstdio>
-#include <string>
 
-using namespace std;
-
-int main () {
-    double poly1[1001], poly2[1001];
-    memset(poly1, 0, sizeof(poly1));
-    memset(poly2, 0, sizeof(poly2));
-    int k = 0;
+int main() {
+    double poly[1001] = {0.0}, result[2001] = {0.0}, an = 0;
+    int k = 0, n = 0, cnt = 0;
     scanf("%d", &k);
     for (int i = 0; i < k; i++) {
-        int t = 0;
-        scanf("%d", &t);
-        scanf("%lf", &poly1[t]);
+        scanf("%d %lf", &n, &an);
+        poly[n] = an;
     }
     scanf("%d", &k);
     for (int i = 0; i < k; i++) {
-        int t = 0;
-        scanf("%d", &t);
-        scanf("%lf", &poly2[t]);
-    }
-    
-    double poly[2001];
-    memset(poly, 0, sizeof(poly));
-    for (int i = 0; i < 1001; i++) {
-        for (int j = 0; j < 1001; j++) {
-            poly[i + j] += poly1[i] * poly2[j];
+        scanf("%d %lf", &n, &an);
+        for (int i = 0; i <= 1000; i++) {
+            result[i+n] += poly[i] * an;
         }
     }
     
-    int cnt = 0;
     for (int i = 2000; i >= 0; i--) {
-        if (poly[i] != 0) {
+        if (result[i] != 0.0) {
             cnt++;
         }
     }
     printf("%d", cnt);
+    
     for (int i = 2000; i >= 0; i--) {
-        if (poly[i] != 0) {
-            printf(" %d %.1f", i, poly[i]);
+        if (result[i] != 0.0) {
+            printf(" %d %.1f", i, result[i]);
         }
     }
+    
     return 0;
 }
