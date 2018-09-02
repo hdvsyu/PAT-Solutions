daeadd6598df343cfc7b85920605da6303f25695
黄忠雨 <hdvsyu@foxmail.com>
2018-09-02 22:51:41 +0800

    refactor: PAT 1025 PAT Ranking（25 分）

--- C++/Advanced/1025. PAT Ranking (25).cpp
+++ C++/Advanced/1025. PAT Ranking (25).cpp
@@ -1,53 +1,49 @@
-#include <iostream>
+#include <cstdio>
 #include <vector>
 #include <cstdlib>
 #include <algorithm>
+#include <cstring>
 using namespace std;
 
-struct test {
-    string number;
-    int score, locnum, locrnk, rnk;
+struct testee {
+    char number[15];
+    int score, locnum, locrnk;
 };
 
-bool cmp(const struct test &a, const struct test &b) {
+bool cmp(testee a, testee b) {
     if (a.score > b.score) return true;
     if (a.score < b.score) return false;
-    return a.number < b.number;
+    return strcmp(a.number, b.number) < 0 ;
 }
 
 int main() {
     int n = 0;
-    cin >> n;
-    vector<struct test> v;
+    scanf("%d", &n);
+    vector<struct testee> v;
     for (int i = 1; i <= n; i++) {
         int k = 0;
-        cin >> k;
-        struct test *arr = new struct test[k];
+        scanf("%d", &k);
         for (int j = 0; j < k; j++) {
-            struct test t;
-            cin >> t.number >> t.score;
+            struct testee t;
+            scanf("%s %d", t.number, &t.score);
             t.locnum = i;
-            arr[j] = t;
+            v.push_back(t);
         }
-        qsort(arr, k, sizeof(arr[0]), [](const void *a, const void *b){
-            const struct test arg1 = *static_cast<const struct test *>(a);
-            const struct test arg2 = *static_cast<const struct test *>(b);
-            
-            if (arg1.score > arg2.score) return -1;
-            if (arg1.score < arg2.score) return 1;
-            return 0;
-        });
-        for (int j = 0; j < k; j++) {
-            arr[j].locrnk = j > 0 && arr[j].score == arr[j - 1].score ?arr[j].locrnk = arr[j - 1].locrnk : j+1;
-            v.push_back(arr[j]);
+        sort(v.end() - k , v.end(), cmp);
+        v[v.size() - k].locrnk = 1;
+        int rank = 2;
+        for (int j = (int)v.size() - k + 1; j < v.size(); j++) {
+            if (v[j].score == v[j-1].score) v[j].locrnk = v[j-1].locrnk;
+            else v[j].locrnk = rank;
+            rank++;
         }
-        delete [] arr;
     }
     sort(v.begin(), v.end(), cmp);
-    cout << v.size() << endl;
+    printf("%lu\n", v.size());
+    int rank = 1;
     for (int i = 0; i < v.size(); i++) {
-        v[i].rnk = i > 0 && v[i].score == v[i - 1].score ? v[i].rnk = v[i - 1].rnk : i + 1;
-        cout << v[i].number << " "<< v[i].rnk << " " << v[i].locnum << " " << v[i].locrnk << endl;
+        if (i > 0 && v[i].score != v[i - 1].score) rank = i+1;
+        printf("%s %d %d %d\n", v[i].number, rank, v[i].locnum, v[i].locrnk);
     }
     return 0;
 }
